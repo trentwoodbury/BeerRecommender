6cf06dd91a60350cf202d644bccadbb4d858eefe
Trent Woodbury <trentwoodbury@gmail.com>
2016-11-23 15:30:42 -0700

    Add storage function

--- WebScraping/get_beer.py
+++ WebScraping/get_beer.py
@@ -1,10 +1,25 @@
 from brewerydb import *
+import json
 
+
+def get_beer(start_page, qty):
+    #input: start page of query, how many pages to query
+    #output: json of beers
+    beers = []
+    for i in range(start_page, start_page+qty):
+        beers.append(BreweryDb.beers({'page' : i}))
+
+
+def store_beer(outfile):
+    #input: outfile, filepath to where resultant json will be stored
+    #output: saves json file.
+    with open(outfile, 'w') as outfile:
+        for beer_dict in beer_list:
+            json.dump(beer_dict, outfile)
 
 if __name__ == "__main__":
     BreweryDb.configure(api_key)
 
-    #we are going to get a json object from the beer database called beers
-    beers = BreweryDb.beers({'abv':"4,10"})
+    beer_list = get_beer(1, 399)
 
-    print beers
+    store_beer('../Data/beerdb.json')
