fd4cd985bfa4111b316ebff822511c6fbe3a96f1
Trent Woodbury <trentwoodbury@gmail.com>
2016-11-25 18:36:18 -0700

    Add format_df function

--- Munging/make_mongo.py
+++ Munging/make_mongo.py
@@ -13,19 +13,26 @@ def make_db(data):
     #Populate database with our dictionaries from data variable
     db = client['beerDB']
     for doc in data:
-        db.beer.insert(doc)
-    print db.beer.find().count()
+        db.beer.insert_one(doc)
+    print "Original Number of Beers:", db.beer.find().count()
     return db
 
 def get_the_good_stuff(db):
     query1 = {'abv': {'$exists': 1}, 'description': {'$exists': 1}, 'ibu': {'$exists': 1}, 'name' : {'$exists': 1}, 'fgMax': {'$exists': 1}}
     query2 = {'abv':1, 'description': 1, 'ibu': 1, 'name': 1, 'fgMax': 1}
     full_info_beers = db.beer.find(query1, query2)
-    print full_info_beers.count()
+    print "Final Number of Beers:", full_info_beers.count()
     return full_info_beers
 
 def to_pandas_we_go(cursor):
-    df = pd.DataFrame(list(cursor))
+    df = pd.DataFrame(list(cursor)).loc[: , ['name', 'description', 'abv', 'fgMax', 'ibu']]
+    print "Sample of Beer DataFrame \n", df.head()
+    return df
+
+def format_df(df):
+    df['abv'] = pd.to_numeric(df['abv'])
+    df['fgMax'] = pd.to_numeric(df['fgMax'])
+    df['ibu'] = pd.to_numeric(df['ibu'])
     return df
 
 if __name__ == "__main__":
@@ -33,10 +40,8 @@ if __name__ == "__main__":
     client = pymongo.MongoClient()
 
     data = get_pickle('../Data/dict_list.pkl')
-    print "All {} pieces of data retrieved".format(len(data))
-    print data[210]
+    print "All {} pieces of data retrieved \n".format(len(data))
 
     db = make_db(data)
     full_info_beers = get_the_good_stuff(db)
-    df = to_pandas_we_go(full_info_beers)
-    
+    df = format_df(to_pandas_we_go(full_info_beers))
