428863d9123bf877b3fe33d0258b89bafd86fcc3
Trent Woodbury <trentwoodbury@gmail.com>
2016-11-25 18:18:45 -0700

    Add get_the_good_stuff()

--- Munging/make_mongo.py
+++ Munging/make_mongo.py
@@ -13,6 +13,14 @@ def make_db(data):
     for doc in data:
         db.beer.insert(doc)
     print db.beer.find().count()
+    return db
+
+def get_the_good_stuff(db):
+    query1 = {'abv': {'$exists': 1}, 'description': {'$exists': 1}, 'ibu': {'$exists': 1}, 'name' : {'$exists': 1}, 'fgMax': {'$exists': 1}}
+    query2 = {'abv':1, 'description': 1, 'ibu': 1, 'name': 1, 'fgMax': 1}
+    full_info_beers = db.beer.find(query1, query2)
+    print full_info_beers.count()
+    return full_info_beers
 
 if __name__ == "__main__":
 
@@ -22,4 +30,5 @@ if __name__ == "__main__":
     print "All {} pieces of data retrieved".format(len(data))
     print data[210]
 
-    make_db(data)
+    db = make_db(data)
+    full_info_beers = get_the_good_stuff(db)
