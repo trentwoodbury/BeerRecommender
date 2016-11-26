84c1bb1417227558a8361e8b41d369a890569df6
Trent Woodbury <trentwoodbury@gmail.com>
2016-11-25 17:33:27 -0700

    Move db size print out of for loop

--- Munging/make_mongo.py
+++ Munging/make_mongo.py
@@ -12,15 +12,14 @@ def make_db(data):
     db = client['beerDB']
     for doc in data:
         db.beer.insert(doc)
-        print db.beer.find().count()
+    print db.beer.find().count()
 
 if __name__ == "__main__":
 
-    client = pymongo.MongoClient('localhost:27017')
+    client = pymongo.MongoClient()
 
     data = get_pickle('../Data/dict_list.pkl')
     print "All {} pieces of data retrieved".format(len(data))
     print data[210]
 
     make_db(data)
-    
