281df0089dcf454a50a2b6eb3af929c36cd07a63
Trent Woodbury <trentwoodbury@gmail.com>
2016-11-25 17:27:09 -0700

    add make_db function

--- Munging/make_mongo.py
+++ Munging/make_mongo.py
@@ -2,11 +2,22 @@ import pymongo
 import pickle
 
 def get_pickle(filepath):
+    #Get data from pickled file
     data = pickle.load(open(filepath))
     return data
 
+def make_db_and_populate(data):
+    #Create MongoDB database called BeerDB
+    #Populate database with our dictionaries from data variable
+    db = client['beerDB']
+    for doc in data:
+        db.beer.insert(doc)
+        print db.beer.find().count()
 
 if __name__ == "__main__":
+
+    client = pymongo.MongoClient('localhost:27017')
+
     data = get_pickle('../Data/dict_list.pkl')
     print "All {} pieces of data retrieved".format(len(data))
     print data[210]
