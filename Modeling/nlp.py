61cc82164c1c4e474bab58eba48c760a63aa32b2
Trent Woodbury <trentwoodbury@gmail.com>
2016-11-30 15:30:02 -0700

    Complete nmf_descriptions function

--- Modeling/nlp.py
+++ Modeling/nlp.py
@@ -21,9 +21,14 @@ def nmf_descriptions(df):
     sw = stopwords.words('english')
     sw.extend(['hoppy', 'beer'])
     tf = TfidfVectorizer(max_df = .9, stop_words = sw).fit_transform(corpus)
+    feature_names = tf.get_feature_names()
 
+    nmf = NMF(n_components = 10, l1_ratio = 0.5).fit(df)
+
+    return nmf, feature_names
 
 
 if __name__ == "__main__":
     df = get_dataframe('../Data/beer_data_final.pkl')
-    nmf_model = nmf_descriptions(df)
+    nmf, feature_names = nmf_descriptions(df)
+    print_top_words(nmf, feature_names, 10 )
