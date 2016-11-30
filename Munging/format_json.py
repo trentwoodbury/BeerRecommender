5a66118d7f1f4de67aa0d0df432ba12717629f14
Trent Woodbury <trentwoodbury@gmail.com>
2016-11-30 10:03:10 -0700

    complete get_the_data function

--- Munging/format_json.py
+++ Munging/format_json.py
@@ -1,14 +1,40 @@
 #The purpose of this program is to convert the disparate
 #.json files into a single dataframe
+
 import yaml
+import os
+
+def get_the_data():
+    dict_list = []
+    counter = 0
+    for filename in os.listdir('../Data'):
+        if filename != '.DS_Store':
+            with open('../Data/'+ str(filename)) as f:
+                for everything in f:
+                    count = 0
+                    everything = everything.split(':')[4:-1]
+                    #Remove brackets before and after dictionary
+                    everything[0] = everything[0][1:]
+                    everything[-1] = everything[-1][:-10]
+                    new_str = ''
+                    layers_deep = 0
+                    for item in everything:
+                        new_str += str(item) + ': '
+                    new_str = new_str.replace('/', '').replace('\\', '').replace('""', '')
+                    #new_str will end with ':' so let's remove that
+                    new_str = new_str[:-2]
+                    dict_list.append(new_str)
+    return dict_list
+
+
+
+
 
 
 
 
 if __name__ == "__main__":
 
-    #dict_list will be a list with all of the dictionaries in it
-    dict_list = []
-    #dict_as_string should be a list of dictionaries read as strings
-    for item in dict_as_string:
-        dict_list.append(yaml.load(eval_doc(item)))
+    dict_list = get_the_data()
+
+    # dict_list.append(yaml.load(new_str))
