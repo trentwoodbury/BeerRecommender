86a4995c8370d7917547e7a93a296cfbd75725d8
Trent Woodbury <trentwoodbury@gmail.com>
2016-11-30 14:40:09 -0700

    Subset using dictionaries and for loop instead of mongoDB

---
+++ Munging/to_pandas_we_go.py
@@ -0,0 +1,38 @@
+import pandas as pd
+import pickle
+import pymongo
+
+
+def get_pickle(filepath):
+    #Get data from pickled file
+    data = pickle.load(open(filepath))
+    subsetted_data = []
+    for d in data:
+        try:
+            d_subset = {'name' : d['name'], 'description': d['style']['description'],
+            'abv': d['abv'], 'ibu': d['ibu'], 'FinalGravity': d['style']['fgMax']}
+            subsetted_data.append(d_subset)
+        except:
+            pass
+    return subsetted_data
+
+def to_pandas_we_go(data):
+    df = pd.DataFrame(data).loc[: , ['name', 'description', 'abv', 'fgMax', 'ibu']]
+    print "Sample of Beer DataFrame \n", df.head()
+    return df
+
+def format_df(df):
+    df['abv'] = pd.to_numeric(df['abv'])
+    df['fgMax'] = pd.to_numeric(df['fgMax'])
+    df['ibu'] = pd.to_numeric(df['ibu'])
+    return df
+
+if __name__ == "__main__":
+
+    client = pymongo.MongoClient()
+
+    data = get_pickle('../Data/beer_data.pkl')
+    df = format_df(to_pandas_we_go(data))
+
+    pickle.dump(data, open('../Data/beer_data_final.pkl', 'w'))
+    print "All {} pieces of data retrieved \n".format(len(data))
