da8b9d30326f09624f1ee43c20ca8049e0bd55f4
Trent Woodbury <trentwoodbury@gmail.com>
2016-12-16 15:40:55 -0700

    Create app.py

---
+++ website/app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, request, session, flash, redirect, url_for, g
+import sqlite3
+
+DATABASE = 'beer_db'
+app = Flask(__name__)
+
+# pulls in app configuration by looking for UPPERCASE variables
+app.config.from_object(__name__)
+
+# function used for connecting to the database
+def connect_db():
+    return sqlite3.connect(app.config['DATABASE'])
+
+@app.route('/')
+    def main():
+        return render_template('homepage.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
