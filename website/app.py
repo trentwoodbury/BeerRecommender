157341e90dce0bfc7dff4cddf2e8d00d4013ea79
Dylan Albrecht <dylan.alb@gmail.com>
2016-12-19 15:28:39 -0700

    fixing template link

--- website/app.py
+++ website/app.py
@@ -13,7 +13,7 @@ def connect_db():
 
 @app.route('/')
 def main():
-    return render_template('bootstrap/index.html')
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
