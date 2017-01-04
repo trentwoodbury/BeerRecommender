9ef6e03b87bb7b0130aea9c16cad31e9b658a38a
Trent Woodbury <trentwoodbury@gmail.com>
2017-01-04 16:40:14 -0700

    Add beer names to homepage

--- website/app.py
+++ website/app.py
@@ -1,6 +1,11 @@
 from flask import Flask, render_template, request, session, flash, redirect, url_for, g
 from flask_bootstrap import Bootstrap
+import pandas as pd
 import sqlite3
+
+def get_beer_names():
+    beer_df =  pd.read_pickle("../Data/beer_data_final.pkl")
+    return list(beer_df['name'])
 
 DATABASE = 'beer_db'
 app = Flask(__name__)
@@ -14,7 +19,25 @@ def connect_db():
 
 @app.route('/')
 def main():
-    return render_template('index.html')
+
+    beers = get_beer_names()
+    beers_split = []
+
+    #Make a list of lists so that we have 5 beers per row
+    for i in range(len(beers)):
+        if i % 5 == 0:
+            beers_split.append([])
+        if len(beers[i]) > 22:
+            beers_split[-1].append(str(beers[i][:19]) + '...')
+        else:
+            beers_split[-1].append(beers[i])
+
+
+    #ensure That our table ends with 5 columns
+    if len(beers_split[-1]) != 5:
+        beers_split = beers_split[:-1]
+
+    return render_template('names_table.html', result=beers_split)
 
 if __name__ == '__main__':
     Bootstrap(app)
